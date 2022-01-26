4c40259e8a333b21efa22204201993d0ee54961b
Walker Davis <walker@wkeithdavis.com>
2022-01-26 17:43:31 -0500

    Everything is completely and royally fucked.

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python
 
-from ctypes import util
-from imp import lock_held
-import json
-from re import I, T, U
-from socketserver import UnixDatagramServer
-from termios import IUCLC
-from xml.etree.ElementInclude import LimitedRecursiveIncludeError
-from numpy import ubyte
 import pandas as pd
 from flask import Flask, request, render_template, redirect, url_for, \
     session, jsonify
@@ -15,15 +7,19 @@ from gamemanager import GameManager as GM
 
 app = Flask(__name__)
 
-def save_player_data(data):
-    df = pd.read_json(data)
-    df.to_csv('playerData.csv')
 
-def load_player_data(uname):
-    data = pd.read_csv('playerData.csv')
-    data['isClient'] = (data['username'] == uname)
-    session['player_data'] = data
-    session['player_json'] = data.to_json(orient='records')
+# helper function to store updated data into the database
+def _save_player_data():
+    print(session['data_json'])
+    # df = pd.read_json(data)
+    # df.to_csv('playerData.csv')
+
+
+# helper function to load player data from the database to the server
+def _load_player_data():
+    df_data = pd.read_csv('playerData.csv')
+    df_data['isClient'] = (df_data['username'] == session['uname'])
+    session['data_json'] = df_data.to_json(orient='records')
 
 
 @app.route('/')
@@ -36,32 +32,32 @@ def load_login():
 def login():
     if (request.method == 'POST'):
         session['uname'] = request.form['username']
-        load_player_data(session['uname'])
+        _load_player_data()
         return redirect(url_for('play'))
     return render_template('login.html')
 
 
 @app.route('/play')
 def play():
-    return render_template('index.html', player_data=session['player_json'])
+    return render_template('index.html', player_data=session['data_json'])
 
 
 @app.route('/play', methods=['POST', 'GET'])
 def handle_move():
     if (request.method == 'POST'):
         move = request.form['move']
+        print(move)
 
-        load_player_data(session['uname'])
-        gm = GM(session['player_data'])
-        player_data = gm.handle_move(move)
-        save_player_data(player_data)
+        _load_player_data()
+        print(session['data_json'])
+        gm = GM(session['data_json'])
+        session['data_json'] = gm.handle_move(move)
+        _save_player_data()
 
-
-        return jsonify({'player_data': player_data})
-    return render_template('index.html', player_data=session['player_json'])
+        return jsonify({'player_data': session['data_json']})
+    return render_template('index.html', player_data=session['data_json'])
 
 
 if __name__ == '__main__':
     app.secret_key = 'key:)'
-    # app.session_cookie_samesite = "lax"
     app.run(debug=True)
--- gamemanager.py
+++ gamemanager.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
 import json
-import pandas as pd
+
+
 class GameManager():
     def __init__(self, playerlist) -> None:
         self.playerlist = playerlist
 
     def handle_move(self, selected_move):
         if (selected_move == "shoot"):
-            pass
+            print('Selected shoot')
         elif (selected_move == "move"):
-            pass
+            print('Selected move')
         elif (selected_move == "range"):
+            print('Selected range')
             for player in self.playerlist:
                 if(player['isClient'] and player['ap'] > 3):
                     player['range'] += 1
                     player['ap'] -= 3
         elif (selected_move == "heal"):
+            print('Selected Heal')
             for player in self.playerlist:
                 if(player['isClient'] and player['ap'] > 3):
                     if(player['hp'] < 3):
                         player['hp'] += 1
                         player['ap'] -= 3
         elif (selected_move == "transfer"):
-            pass
+            print('Selected Transfer')
 
         return json.dumps(self.playerlist)
